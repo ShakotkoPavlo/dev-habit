0ca55cda3fea9419262dcd95aab7bc22c33f2416
Pavel Shakotko <pavel.shakotko@budgetthuis.nl>
2025-11-27 11:14:49 +0100

    added entity and db

---
+++ DevHabit/DevHabit.Api/Database/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using DevHabit.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevHabit.Api.Database;
+
+public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Habit> Habits { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema(Schemas.Application);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
+
+public static class Schemas
+{
+    public const string Application = "dev_habit";
+}
---
+++ DevHabit/DevHabit.Api/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,26 @@
+﻿using DevHabit.Api.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevHabit.Api.Extensions;
+
+public static class DatabaseExtensions
+{
+    public static async Task ApplyMigrationsAsync(this WebApplication webApplication)
+    {
+        using IServiceScope scope = webApplication.Services.CreateScope();
+
+        await using ApplicationDbContext dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        try
+        {
+            await dbContext.Database.MigrateAsync();
+
+            webApplication.Logger.LogInformation("Migration applied successfully!");
+        }
+        catch (Exception e)
+        {
+            webApplication.Logger.LogError(e, "Migration not applied!");
+            throw;
+        }
+    }
+}
